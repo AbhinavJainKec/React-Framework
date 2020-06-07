9b6c7508ef882f9496adcde1d6702f4b403ae7bd
Abhinav Jain <abhinav.jain@krishnacollege.ac.in>
2020-06-08 00:56:48 +0530

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
 /*
 function App() {
@@ -19,6 +20,15 @@ function App() {
 */
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div>
@@ -27,7 +37,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
