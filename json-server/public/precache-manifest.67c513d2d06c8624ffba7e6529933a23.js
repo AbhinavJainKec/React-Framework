a9b1294d8c6c50936299d0aaf5ce0d26dd07740d
Abhinav Jain <abhinav.jain@krishnacollege.ac.in>
2020-06-24 04:42:34 +0530

    Project Build

---
+++ json-server/public/precache-manifest.67c513d2d06c8624ffba7e6529933a23.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18294fa1fa40ace3014bb3dea67d97f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a28785faf0ec7b780d0",
+    "url": "/static/css/2.1bd367ea.chunk.css"
+  },
+  {
+    "revision": "6fb579f2429538506807",
+    "url": "/static/css/main.e0dbd7ea.chunk.css"
+  },
+  {
+    "revision": "8a28785faf0ec7b780d0",
+    "url": "/static/js/2.d8696f44.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d8696f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fb579f2429538506807",
+    "url": "/static/js/main.4e9104bb.chunk.js"
+  },
+  {
+    "revision": "79298229a104881fd483",
+    "url": "/static/js/runtime-main.ad013ee6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
